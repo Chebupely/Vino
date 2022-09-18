8745682528ece302fb0e101bfb62e256c1a8ac6c
Woodey777 <79203172+Woodey777@users.noreply.github.com>
2022-09-18 09:12:07 +0300

    Some design changes

--- Agro/main/urls.py
+++ Agro/main/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about'),
+    path('findVino', views.findvino, name='findVino'),
+    path('res', views.res, name='result'),
     path('', views.about1, name='about1'),
     path('about', views.about2, name='about2')
 ]
--- Agro/main/views.py
+++ Agro/main/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main/index.html')
 
-def about(request):
-    return render(request, 'main/about.html')
+def findvino(request):
+    return render(request, 'main/findVino.html')
+
+def res(request):
+    if request.method == 'POST':
+        return render(request, 'main/res.html')
 
 def about1(request):
     return render(request, 'main/about1.html')
