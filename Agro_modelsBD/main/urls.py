3339efead5cc0b17e9476411095fd8ffe76646ef
AlexErmakov77git config user.name git config --global user.name AlexErmakov77 <aaermakov77@gmail.com>
2022-09-18 09:54:03 +0300

    1

--- Agro_modelsBD/main/urls.py
+++ Agro_modelsBD/main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    # path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('', views.about1, name='about1'),
-    path('about', views.about2, name='about2')
+    path('about', views.about2, name='about2'),
+    path('', views.vinograd, name='vinograd'),
+    path('create/', views.create)
 ]
--- Agro_modelsBD/main/views.py
+++ Agro_modelsBD/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'main/index.html')
+# def index(request):
+#     return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')
@@ -12,4 +12,22 @@ def about1(request):
 
 def about2(request):
     return render(request, 'main/about2.html')
+
+def vinograd(request):
+    return render(request, 'main/vinograd.html')
+
+def vinogradnikrequest():
+    vineyardBD = vinogradnik.object.all()
+    return render(request, "main/vinograd.html", {"vineyardBD": vineyardBD})
+
+def create(request):
+    if request.metod == "POST":
+        vineyard = vinogradnik()
+        vineyard.name = request.POST.get("name_Vinogradnik")
+        vineyard.number = request.POST.get("number_Vinogradnik")
+        vineyard.shirota = request.POST.get("shirota_Vinogradnik")
+        vineyard.dolgota = request.POST.get("dolgota_Vinogradnik")
+        vineyard.description = request.POST.get("description_Vinogradnik")
+        vineyard.save()
+    return HttpResponseRedirect("/")
 
